068f60dd8588224daa805e60e4c46ffa57604c4b
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-08 11:03:50 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,15 +5,10 @@
 
 
 /**
- *
  *  main - prints exactly "and that piece of art is useful"
- *
  *  - Dora Korpar, 2015-10-19",
- *
  *  followed by a new line, to the standard error.
- *
  *  Return: Always 0 (Success)
- *
  */
 
 int main(void)
