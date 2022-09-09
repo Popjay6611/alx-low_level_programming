c7e5fb19e8803ce12bb9bede44f3d72fdb1c3238
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-09 13:58:20 -0700

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *main -> assign a random number to the variable n each time it is execute
  *and print the last digit of the number stored in the variable n
@@ -16,7 +13,7 @@ for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
 }
-putchar('\n'); 
-return (0);
+putchar('\n');
+  return (0);
 
 }
