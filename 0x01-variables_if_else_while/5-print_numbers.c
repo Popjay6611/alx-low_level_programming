36ba913713f388f30ddc2cc906adcf06baa8aab1
popoola <popjay35@gmail.com>
2022-09-09 03:59:19 -0700

    c programming project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- all single digits number of base 10
+ *result return(sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+         for(i =0; i <=10; i++);
+	 
+		 printf("%d", i);
+	 printf("\n");
+	 return (0);
+}
