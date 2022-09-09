0054dc5c22fc7757a6b82fca5a63330cca124e24
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-09 04:12:18 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,12 +3,9 @@
 
 
 /**
- *
- *  * main - prints all possible different combinations of three digits
- *
- *   * Return: Always 0 (Success)
- *
- *    */
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
