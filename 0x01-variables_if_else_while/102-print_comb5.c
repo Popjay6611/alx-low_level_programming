822e850e7714fb074cb64d536bbcceb7879a8566
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-09 14:48:34 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main -> Write a program that prints all possible combinations of two two-digit numbers.
+*main -> that prints all possible combinations of two two-digit numbers.
 *Return: Always 0 (Success)
 */
 
