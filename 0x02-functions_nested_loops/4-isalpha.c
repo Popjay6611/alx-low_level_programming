8b471f1d7fc329ad967de86e05d5494cad57f557
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-13 11:29:07 -0700

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - check for lower case letter
- * @c : character to check the case
+ * _isalpha - check for lower case letter
+ * @c : character to check
  * Return:0 or 1
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	return (c >= 97 && c <= 122);
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 
 }
