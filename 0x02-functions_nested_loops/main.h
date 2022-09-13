b272cdee5c6eea6d27945b3d6311ccfc1679bc95
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-13 09:01:58 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,13 +13,3 @@ int print_sign(int n);
 int _abs(int n);
 
 int print_last_digit(int n);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n)
