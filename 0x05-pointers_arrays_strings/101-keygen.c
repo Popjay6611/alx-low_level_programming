3614d59cf80e54f63436e65c0e851c3deb87ec45
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-09-19 13:29:16 -0700

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,15 @@
-rr
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
