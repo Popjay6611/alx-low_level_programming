fbc57c91207a42af28f87870ed11bcb52a9ab78e
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-06 09:52:35 -0700

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,19 @@
-off
+#include "main.h"
+/**
+ *malloc_checked - array for prints a string
+ *@b: number of memory
+ *Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+
+return (p);
+}
