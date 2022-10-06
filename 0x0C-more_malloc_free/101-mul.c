06e79c800a4989b7750bc096703133767102a9a5
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-06 13:39:17 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,19 +11,19 @@ int main(int argc, char *argv[])
 unsigned long mul;
 int i, j;
 	if (argc != 3)
-	{ printf("Error\n");
+	{ _putchar("Error\n");
 	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
+			{  _putchar("Error\n");
 			exit(98); }
 		}
 
 	}
 	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
+	_putchar("%lu\n", mul);
 return (0);
 }
