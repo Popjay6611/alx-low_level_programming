775aebab688f721f31547b131267a37d44b87461
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-10 12:54:05 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,11 @@
-oo
+#include <stdio.h>
+/**
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
