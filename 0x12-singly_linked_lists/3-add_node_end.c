dded0c38286b935637a190ab01c52d5c1202fac4
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-20 13:45:45 -0700

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,45 @@
 #include "lists.h"
-
+#include <string.h>
 /**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
+
 {
-    list_t *new, *temp;
-    size_t nchar;
+char *dup;
+int len;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+list_t *new, *last;
 
-    new->str = strdup(str);
-
-    for (nchar = 0; str[nchar]; nchar++)
-        ;
-
-    new->len = nchar;
-    new->next = NULL;
-    temp = *head;
-
-    if (temp == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = new;
-    }
-
-    return (*head);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+dup = strdup(str);
+if (str == NULL)
+{
+free(new);
+return (NULL);
+}
+for (len = 0; str[len];)
+len++;
+new->str = dup;
+new->len = len;
+new->next = NULL;
+if (*head == NULL)
+*head = new;
+else
+{
+last = *head;
+while (last->next != NULL)
+last = last->next;
+last->next = new;
+}
+return (*head);
 }
