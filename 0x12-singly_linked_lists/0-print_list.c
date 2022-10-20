f76f8c07008be4c3c3ff6dad47200c193f39dd19
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-20 13:37:22 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h
- */
+* print_list - Prints all the elements of a list_t list.
+* @h: The list_t list.
+*
+* Return: The number of nodes in h
+*/
 
 size_t print_list(const list_t *h)
 
