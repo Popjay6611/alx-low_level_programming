c4daafe5a9ebd0350cddaa579a13b74e7c9edf6b
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-20 13:50:32 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+void __attribute__((constructor)) hare(void);
 
 /**
- * bmain - function executed before main
- * Return: no return.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
 
-void __attribute__((constructor)) bmain()
+void hare(void)
+
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+
+printf("You're beat! and yet, you must allow,\n"
+
+"I bore my house upon my back!\n");
+
 }
