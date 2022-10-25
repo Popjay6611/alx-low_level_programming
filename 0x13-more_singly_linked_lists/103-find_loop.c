f718fdd983c696282b1831b0f10ae7596e30d427
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-25 07:59:50 -0700

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,33 +2,32 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: 
- *
+ * @head:
  * Return: address of the node where the loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+listint_t *slow = head;
+listint_t *fast = head;
 
-    if (!head)
-        return (NULL);
+if (!head)
+return (NULL);
 
-    while (slow && fast && fast->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        if (fast == slow)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (fast);
-        }
-    }
+while (slow && fast && fast->next)
+{
+fast = fast->next->next;
+slow = slow->next;
+if (fast == slow)
+{
+slow = head
+while (slow != fast)
+{
+slow = slow->next;
+fast = fast->next;
+}
+return (fast);
+}
+}
 
-    return (NULL);
+return (NULL);
 }
