6aca8c8744d09c863898c4c66d57c880707ef5c6
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-10-25 08:01:46 -0700

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,7 +2,7 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head:
+ * @head:linked list to search for
  * Return: address of the node where the loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
